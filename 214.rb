7c018e46cc33c712fdc1e1a92ac0dc8345782afc
Neal Lindsay <neal.lindsay@gmail.com>
2008-10-30 14:56:36 -0400

    Still working on 214

--- 214.rb
+++ 214.rb
@@ -13,6 +13,12 @@
 starttime = Time.now
 puts starttime.strftime('%Y-%m-%d %H:%M:%S')
 
+# $primefactors[n] should return an array of all
+# prime factors of n (including n if applicable)
+$primefactors = Hash.new do |h,key|
+	h[key] = something
+end
+
 def totient(n)
 	# number of k where 1 <= k < n and n shares no
 	# common factors with k other than one
@@ -20,11 +26,27 @@ def totient(n)
 	# factor n
 	# find all multiples < n of the factors
 	# remove all those multiples from the set 1..n-1
-	# count set members - that's your k
+	# count remaining set members - that's your k
 end
 
-def antitotient(k)
-	# returns the set of all n such that totient(n) == k
+$chainlen = Hash.new do |h,key|
+	h[key] = 1 + $chainlen[totient(key)]
+end
+$chainlen[1] = 1
+
+primesum = 0
+
+# all primes > 3 are 6x +/- 1
+# we don't need to bother testing multiples of 2 or 3
+(1..6666666).each do |i|
+	if ($primefactors[i*6-1].length == 1) and ($chainlen[i*6-1] == 25)
+		primesum += i*6-1
+		puts i*6-1
+	end
+	if ($primefactors[i*6+1].length == 1) and ($chainlen[i*6+1] == 25)
+		primesum += i*6+1
+		puts i*6+1
+	end
 end
 
 puts 'the answer'
