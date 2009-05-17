7c488eda00466f035aafc0f6f0b81489d73cff84
Neal Lindsay <neal.lindsay@gmail.com>
2009-05-17 07:52:50 -0400

    made 003.erl a little more efficient

--- 003.erl
+++ 003.erl
@@ -5,7 +5,7 @@ start() -> io:format("Answer: ~B~n",[maxfactor(600851475143)]).
 
 maxfactor(Target) -> maxfactor(2, 1, Target).
 
-maxfactor(Try, _, Target) when Try >= Target -> Target;
+maxfactor(Try, Max, Target) when Try * Max >= Target -> Target;
 maxfactor(Try, _, Target) when Target rem Try == 0 ->
   maxfactor(Try, Try, Target div Try);
 maxfactor(2, Max, Target) when Target rem 2 > 0 ->
