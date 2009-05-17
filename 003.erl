2a207c8142fe13ef3a27ea05ae5ac7ee850fb108
Neal Lindsay <neal.lindsay@gmail.com>
2009-05-17 01:23:42 -0400

    solved 3 in erlang

---
+++ 003.erl
@@ -0,0 +1,14 @@
+-module('003').
+-export([start/0]).
+
+start() -> io:format("Answer: ~B~n",[maxfactor(600851475143)]).
+
+maxfactor(Target) -> maxfactor(2, 1, Target).
+
+maxfactor(Try, _, Target) when Try >= Target -> Target;
+maxfactor(Try, _, Target) when Target rem Try == 0 ->
+  maxfactor(Try, Try, Target div Try);
+maxfactor(2, Max, Target) when Target rem 2 > 0 ->
+  maxfactor(3, Max, Target);
+maxfactor(Try, Max, Target) when Target rem Try > 0 ->
+  maxfactor(Try + 2, Max, Target).
