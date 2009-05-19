d06501e7dc73e7788d15857bc21c2c7b8c419153
Neal Lindsay <neal.lindsay@gmail.com>
2009-05-19 08:15:19 -0400

    Commented code and got rid of the "Max" parameter.
    "Max" was useless - it was left over from a previous implementation.

--- 003.erl
+++ 003.erl
@@ -1,12 +1,30 @@
 -module('003').
 -export([start/0]).
 
+% What is the largest prime factor of the number 600851475143?
+%
+% Strategy:
+% Factor out prime numbers starting with the lowest.
+% Divide by the factors as we find them.
+% When no more factorization can be done, the result is the
+% largest prime factor.
+
 start() -> io:format("Answer: ~B~n",[maxfactor(600851475143)]).
 
-maxfactor(Target) -> maxfactor(2, 1, Target).
+% Start by checking the lowest prime: 2
+maxfactor(Target) -> maxfactor(2, Target).
 
-maxfactor(Try, _, Target) when Try * Try > Target -> Target;
-maxfactor(Try, _, Target) when Target rem Try == 0 ->
-  maxfactor(Try, Try, Target div Try);
-maxfactor(2, Max, Target) -> maxfactor(3, Max, Target);
-maxfactor(Try, Max, Target) -> maxfactor(Try + 2, Max, Target).
+
+% when the factor we are trying is > the square root of
+% Target, Target is the largest prime factor
+maxfactor(Try, Target) when Try * Try > Target -> Target;
+
+% When we find a factor, factor it out and try it again
+maxfactor(Try, Target) when Target rem Try == 0 ->
+  maxfactor(Try, Target div Try);
+
+% After we factor out any 2s, start us off on odd numbers
+maxfactor(2, Target) -> maxfactor(3, Target);
+
+% Try the next odd number (easier than figuring out primes)
+maxfactor(Try, Target) -> maxfactor(Try + 2, Target).
